024bb25d36e637cad7b6a0da36a6859e08162eaf
a7md5ater <ahmedkhater142000@gmail.com>
2024-04-20 09:54:05 +0200

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:kakas/app/injector.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -8,6 +9,7 @@ import 'package:kakas/app/bloc_observer.dart';
 //
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await ScreenUtil.ensureScreenSize();
   setupGetIt();
   Bloc.observer = MyBlocObserver();
   runApp(const MyApp());
